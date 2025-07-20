36885cd1926c641cb03c9561e36f07a9df526bda
Dineslim <support@reputia.fr>
2025-07-20 12:58:51 +0200

    Ajout Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react" // Import du module Analytics
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> {/* Active le tracking Vercel */}
+    </>
+  )
 }
